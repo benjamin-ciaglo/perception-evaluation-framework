37010d781b2189fe42d0c26a0dc1abe0c1383805
benjamin-ciaglo <102775306+benjamin-ciaglo@users.noreply.github.com>
2023-05-27 16:15:03 -0400

    url correction
    url correction

--- speak-tool/app.py
+++ speak-tool/app.py
@@ -209,7 +209,7 @@ def evaluate(proctor_name, battery_name, test_idx, question_idx):
 	print('evaluate: ')
 	print('ass_id: ', ass_id, ' hit_id: ', hit_id, ' submit_path: ', ' worker_id: ', worker_id)
 	audioFile = worker_id + "_" + ass_id + "_synthesized.mp3"
-	audioURL = 'https://percepteval.net' + url_for('static', filename=audioFile)
+	audioURL = 'https://percepteval.net' + '/' + audioFile
 	print(audioURL)
 	submitEvaluation = '/' + proctor_name + '/' + battery_name + '/thanks/' + test_idx + '/' + question_idx + arg_string
 	return render_template(evaluation_template,
