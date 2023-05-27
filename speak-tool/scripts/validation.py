1e12dd276f53a7bce2cfae788ac19959a3cd8d5c
benjamin-ciaglo <102775306+benjamin-ciaglo@users.noreply.github.com>
2023-05-27 18:15:59 -0400

    misalignment fix
    misalignment fix

--- speak-tool/scripts/validation.py
+++ speak-tool/scripts/validation.py
@@ -20,7 +20,7 @@ def val1(audio_file):
 	with sr.AudioFile(audio_file) as source:
     		audio = r.record(source)
 
-	with open(os.path.splitext(audio_file)[0] + '_transcript.txt', 'w') as output:
+	with open(os.path.splitext(audio_file)[0] + '_worker_transcript.txt', 'w') as output:
 		try:
 			transc = r.recognize_google(audio)
 			output.write(transc)
