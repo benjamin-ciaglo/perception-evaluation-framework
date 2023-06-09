1861bf8d77513605049a5efee69ec978cab19a2a
benjamin-ciaglo <102775306+benjamin-ciaglo@users.noreply.github.com>
2023-06-09 03:03:44 -0400

    fix arg_string
    fix arg_string

--- speak-tool/app.py
+++ speak-tool/app.py
@@ -10,6 +10,7 @@ import time
 import werkzeug
 import geoip2.database
 import boto3
+import re
 # helpers
 import scripts
 from ast import literal_eval
@@ -100,7 +101,7 @@ def init_test(proctor_name, battery_name, test_idx):
 		elif worker_already_uploaded_audio_and_got_response:
 			with open(os.path.join(save_location, env, worker_id + ".txt"), 'r') as rf:
 				prev_ass_id = rf.readline().strip('\n')
-			ass_id = prev_ass_id
+			arg_string = re.sub('rassignmentId=.*?&', prev_ass_id, arg_string)
 			nextPage = '/' + proctor_name + '/' + battery_name + '/evaluate/' + test_idx + '/0/0' + arg_string
 			session_file = os.path.join(save_location, env, worker_id + "_" + "session" ".txt")
 			with open(session_file, 'r') as rf:
@@ -109,7 +110,7 @@ def init_test(proctor_name, battery_name, test_idx):
 		elif worker_already_started_this_task:
 			with open(os.path.join(save_location, env, worker_id + ".txt"), 'r') as rf:
 				prev_ass_id = rf.readline().strip('\n')
-			ass_id = prev_ass_id
+			arg_string = re.sub('rassignmentId=.*?&', prev_ass_id, arg_string)
 			nextPage = '/' + proctor_name + '/' + battery_name + '/record-voice/' + test_idx + '/0/1' + arg_string
 			session_file = os.path.join(save_location, env, worker_id + "_" + "session" ".txt")
 			with open(session_file, 'r') as rf:
