cd688caf340ecdb62a40dda7c2cfab2607174673
benjamin-ciaglo <102775306+benjamin-ciaglo@users.noreply.github.com>
2023-06-13 01:29:49 -0400

    removing broken ref
    removing broken ref

--- speak-tool/app.py
+++ speak-tool/app.py
@@ -555,9 +555,7 @@ def display_code(proctor_name, battery_name, test_idx, question_idx):
 		with open(code_pickle, 'rb') as f:
 			codes = pickle.load(f)
 		code1, code2, code3, code4, code5, code6, code7, code8 = codes
-		code_filename = os.path.join(save_location,env,worker_id+"_"+ass_id+"_code.txt")
-		with open(code_filename, 'w') as wf:
-			wf.write(code_displayed_to_worker)
+
 		return render_template(code_template,
 			code1=code1,
 			code2=code2,
